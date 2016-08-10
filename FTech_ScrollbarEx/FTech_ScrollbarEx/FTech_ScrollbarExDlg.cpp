e63e1859a08dd23ea3dd746a2be2172ff084e4ea
luckygg <kgg124@naver.com>
2016-08-10 17:55:10 +0900

    1st upload.

---
+++ FTech_ScrollbarEx/FTech_ScrollbarEx/FTech_ScrollbarExDlg.cpp
@@ -0,0 +1,127 @@
+
+// FTech_ScrollbarExDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "FTech_ScrollbarEx.h"
+#include "FTech_ScrollbarExDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CFTech_ScrollbarExDlg dialog
+
+
+
+
+CFTech_ScrollbarExDlg::CFTech_ScrollbarExDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CFTech_ScrollbarExDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CFTech_ScrollbarExDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_SCROLLBAR1, m_sbTest1);
+	DDX_Control(pDX, IDC_SCROLLBAR2, m_sbTest2);
+}
+
+BEGIN_MESSAGE_MAP(CFTech_ScrollbarExDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_HSCROLL()
+	ON_WM_VSCROLL()
+END_MESSAGE_MAP()
+
+
+// CFTech_ScrollbarExDlg message handlers
+
+BOOL CFTech_ScrollbarExDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	m_sbTest1.InitControl(this);
+	m_sbTest1.SetScrollRange(0,100);
+	m_sbTest1.SetScrollPos(50);
+	SetDlgItemInt(IDC_LbInfo1,50);
+	m_sbTest1.SetColorChannel(255,255,0,0);
+	m_sbTest1.SetColorArrow(255,0,255,0,255,0,255,0);
+	m_sbTest1.SetEnable(false);
+
+	m_sbTest2.InitControl(this,false);
+	m_sbTest2.SetScrollRange(0,100);
+	m_sbTest2.SetScrollPos(0);
+	SetDlgItemInt(IDC_LbInfo2,0);
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CFTech_ScrollbarExDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CFTech_ScrollbarExDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CFTech_ScrollbarExDlg::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	if (pScrollBar->m_hWnd == m_sbTest1.m_hWnd)
+	{
+		int pos = m_sbTest1.GetScrollPos();
+		SetDlgItemInt(IDC_LbInfo1,pos);
+	}
+
+	CDialogEx::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CFTech_ScrollbarExDlg::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	if (pScrollBar->m_hWnd == m_sbTest2.m_hWnd)
+	{
+		int pos = m_sbTest2.GetScrollPos();
+		SetDlgItemInt(IDC_LbInfo2,pos);
+	}
+
+	CDialogEx::OnVScroll(nSBCode, nPos, pScrollBar);
+}
---
+++ FTech_ScrollbarEx/FTech_ScrollbarEx/FTech_ScrollbarExDlg.h
@@ -0,0 +1,36 @@
+
+// FTech_ScrollbarExDlg.h : header file
+//
+
+#pragma once
+#include "ScrollbarEx.h"
+
+// CFTech_ScrollbarExDlg dialog
+class CFTech_ScrollbarExDlg : public CDialogEx
+{
+// Construction
+public:
+	CFTech_ScrollbarExDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_FTECH_SCROLLBAREX_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CScrollBarEx m_sbTest1;
+	CScrollBarEx m_sbTest2;
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+};
